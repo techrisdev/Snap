aff4bac681a348f7d61e5b9e5fc3ad3b17c097f3
techrisdev <techrisdev>
2021-03-08 13:53:20 +0100

    Initial Commit

---
+++ Snap/ContentView.swift
@@ -0,0 +1,19 @@
+// ContentView.swift
+//
+// Created by TeChris on 08.03.21.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
